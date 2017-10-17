b35d27d4415f5a51afdd436e699ddc44b7d9f99d
hning86 <haining@microsoft.com>
2017-10-17 17:14:46 -0400

    almost working

--- Controllers/FeedbackController.cs
+++ Controllers/FeedbackController.cs
@@ -1,20 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ViennaFeedback.Models;
 
-namespace ViennaFeedback.Controllers 
+namespace ViennaFeedback.Controllers
 {
-    public class FeedbackController : Controller 
+    public class FeedbackController : Controller
     {
-        // GET: /Feedback
-        // Show a feedback list
-        public IActionResult Index()
+        private readonly MvcFeedbackContext _context;
+
+        public FeedbackController(MvcFeedbackContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Feedback
+        public async Task<IActionResult> Index(DateTime? dt) {
+            if (!dt.HasValue)
+                dt = DateTime.Now;
+            Console.WriteLine(dt);
+            return View(await _context.Feedback
+                .Where(f => f.eventTime >= dt.Value.Date && f.eventTime < dt.Value.Date.AddDays(1).AddTicks(-1))
+                .OrderBy(f => f.eventTime)
+                .ToListAsync());
+        }
+
+        // GET: Feedback/Details/5
+        public async Task<IActionResult> Details(DateTime? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.Feedback
+                .SingleOrDefaultAsync(m => m.eventTime == id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedback);
+        }
+
+        // GET: Feedback/Create
+        public IActionResult Create()
         {
             return View();
         }
-        // GET: /Feedback/Welcome
-        public string Welcome(string name, int numTimes = 1)
+
+        // POST: Feedback/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("eventTime,subject")] Feedback feedback)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            if (ModelState.IsValid)
+            {
+                _context.Add(feedback);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(feedback);
+        }
+
+        // GET: Feedback/Edit/5
+        public async Task<IActionResult> Edit(DateTime? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.Feedback.SingleOrDefaultAsync(m => m.eventTime == id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+            return View(feedback);
+        }
+
+        // POST: Feedback/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(DateTime id, [Bind("eventTime,subject")] Feedback feedback)
+        {
+            if (id != feedback.eventTime)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(feedback);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FeedbackExists(feedback.eventTime))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(feedback);
+        }
+
+        // GET: Feedback/Delete/5
+        public async Task<IActionResult> Delete(DateTime? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.Feedback
+                .SingleOrDefaultAsync(m => m.eventTime == id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedback);
+        }
+
+        // POST: Feedback/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(DateTime id)
+        {
+            var feedback = await _context.Feedback.SingleOrDefaultAsync(m => m.eventTime == id);
+            _context.Feedback.Remove(feedback);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FeedbackExists(DateTime id)
+        {
+            return _context.Feedback.Any(e => e.eventTime == id);
         }
     }
 }
--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,9 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ViennaFeedback.Models
 {
     public class Feedback
     {
-        public string subject { get; set; }
+        [Key]
+        public DateTime eventTime {get; set;}
+        public string appVersion { get; set;}
+        public string clientIP { get; set; }
+        public string message { get; set; }
+        public string feedbackType { get; set; }
+        public string email { get; set; }
+        public string screenshot { get; set; }
     }
 }
